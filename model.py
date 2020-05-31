9ab28311f984a6cbb49526a96485704cd89a6b5e
Renata Palyafari <palyafarirenata@gmail.com>
2020-05-31 12:50:26 +0200

    Implement Dueling DQN algorithm

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=32):
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -26,3 +26,46 @@ class QNetwork(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
+
+class DuelingQNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=64, fc2_units=64, fcvs_units=32, fcas_units=32):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+            fc1_units (int): Number of nodes in first hidden layer
+            fc2_units (int): Number of nodes in second hidden layer
+        """
+        super(DuelingQNetwork, self).__init__()
+        
+        self.feauture_layer = nn.Sequential(
+            nn.Linear(state_size, fc1_units),
+            nn.ReLU(),
+            nn.Linear(fc1_units, fc2_units),
+            nn.ReLU()
+        )
+
+        self.value_stream = nn.Sequential(
+            nn.Linear(fc2_units, fcvs_units),
+            nn.ReLU(),
+            nn.Linear(fcvs_units, 1)
+        )
+
+        self.advantage_stream = nn.Sequential(
+            nn.Linear(fc2_units, fcas_units),
+            nn.ReLU(),
+            nn.Linear(fcas_units, action_size)
+        )
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        features = self.feauture_layer(state)
+        values = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        q_values = values + (advantages - advantages.mean())
+
+        return q_values
